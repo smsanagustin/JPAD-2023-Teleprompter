2050ede43e4fac528f722cce4870101c863491a8
Your Name <you@example.com>
2023-06-21 11:51:02 +0800

    feat: React development file structure is ready

---
+++ src/pages/ShowcaseComponents.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// start importing components from this line onwards
+
+
+
+function ShowcaseComponents(){
+    return <>
+        {/* add components below */}
+    </>
+}
+
+export default ShowcaseComponents
---
+++ src/pages/Teleprompter.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Teleprompter(){
+    {console.log("Teleprompter is rendered")}
+    return <div>
+            This is a text.
+        </div>
+}
+
+export default Teleprompter
