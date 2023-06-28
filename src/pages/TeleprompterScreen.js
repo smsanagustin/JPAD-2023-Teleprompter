a88d4fa300a425aebb6d01b8f88dccba28010e9c
frankstef <famaddela@up.edu.ph>
2023-06-28 15:53:54 +0800

    ui: wrapping for teleprompter screen

--- src/pages/TeleprompterScreen.js
+++ src/pages/TeleprompterScreen.js
@@ -1,21 +1,25 @@
 import { useNavigate, useLocation } from "react-router";
-// import "../assets/stylesheets/teleprompter.css";
+// import {pre} from "../assets/stylesheets/teleprompter.css";
 
 
 export default function TeleprompterScreen() {
     const navigate = useNavigate();
     const location = useLocation();
     const state = location.state;
+    const rootStyle = {
+        backgroundColor: state.bg,
+      };
 
 
     return (
-        <>
+        <div style={rootStyle}>
             <p>Teleprompter Screen</p>
             <button onClick={() => navigate(-1)}>Back</button>
-            <pre style={{ color: state.color, fontSize: state.fontsize}}>{state.script}</pre>
+            <pre style={{ color: state.color, fontSize: state.fontsize, whiteSpace: 'pre-wrap',
+        wordWrap: 'break-word'}}>{state.script}</pre>
             <button>start</button>
             <button>pause</button>
             <button>stop</button>
-        </>
+        </div>
     );
 }
